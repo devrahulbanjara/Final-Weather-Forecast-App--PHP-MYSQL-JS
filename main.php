dfe13a8cb7f8ce7a0a069cfdfd53f9fbb5250022
Rahul Dev Banjara <rdbanjara07@gmail.com>
2024-02-22 09:45:04 +0545

    updated

--- main.php
+++ main.php
@@ -1,9 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://errors.infinityfree.net");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Headers: Content-Type");
-
 function fetchWeatherDataFromAPI($city)
 {
     $apiKey = "145c5f5cc7b6719079c76a215871e298";
@@ -19,6 +15,7 @@ function fetchWeatherDataFromAPI($city)
     return false;
 }
 
+//gets UTC time and adjusts it to kathmandu's time
 function getCurrentTimestampInHours()
 {
     $current_utc_time = gmdate("H:i");
@@ -33,19 +30,18 @@ function getWeatherDataFromDatabase($conn, $city)
     date_default_timezone_set('Asia/Kathmandu');
     $todayString = new DateTime();
     $today = $todayString->format('Y-m-d');
-    
+    //query to return a row of today's date
     $existingQuery = "SELECT * FROM weather_details WHERE city_name='$city' AND DATE(weather_date) = '$today' ORDER BY weather_date DESC LIMIT 1";
     $result = $conn->query($existingQuery);
-
     return ($result && $result->num_rows > 0) ? $result->fetch_assoc() : false;
 }
 
 function getHistoricalWeatherData($conn, $city)
 {
     $today = date("Y-m-d");
+    //return latest dates first and it must be less than today's date
     $historyQuery = "SELECT * FROM weather_details WHERE city_name='$city' AND weather_date < '$today' ORDER BY weather_date DESC LIMIT 7";
     $historyResult = $conn->query($historyQuery);
-
     $historyData = [];
     while ($historyRow = $historyResult->fetch_assoc()) {
         $entry = [
@@ -56,7 +52,6 @@ function getHistoricalWeatherData($conn, $city)
         ];
         $historyData[] = $entry;
     }
-
     return $historyData;
 }
 
@@ -65,15 +60,15 @@ function updateDatabaseWithAPIData($conn, $city, $apiData, $currentHourInNepal)
     date_default_timezone_set('Asia/Kathmandu');
     $todayString = new DateTime();
     $todate = $todayString->format('Y-m-d');
-    
+    //updates the row with latest data
     $country = $apiData["sys"]["country"];
-    $updateQuery = "UPDATE weather_details SET 
-                temperature='{$apiData["main"]["temp"]}', 
-                description='{$apiData["weather"][0]["description"]}', 
-                timezone='{$apiData["timezone"]}', 
-                humidity='{$apiData["main"]["humidity"]}', 
-                wind='{$apiData["wind"]["speed"]}', 
-                pressure='{$apiData["main"]["pressure"]}', 
+    $updateQuery = "UPDATE weather_details SET
+                temperature='{$apiData["main"]["temp"]}',
+                description='{$apiData["weather"][0]["description"]}',
+                timezone='{$apiData["timezone"]}',
+                humidity='{$apiData["main"]["humidity"]}',
+                wind='{$apiData["wind"]["speed"]}',
+                pressure='{$apiData["main"]["pressure"]}',
                 icon='{$apiData["weather"][0]["icon"]}',
                 data_stored_hour='{$currentHourInNepal}',
                 country= '{$country}'
@@ -94,16 +89,19 @@ function insertDataIntoDatabase($conn, $city, $apiData, $currentHourInNepal)
     $conn->query($insertQuery);
 }
 
+
+
+
+//default request method = get
 if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["city"])) {
     $city = $_GET["city"];
 
-    $conn = new mysqli("localhost", "root", "", "weather");
+    $conn = new mysqli("mysql2.serv00.com", "m3001_devrahul", "Apple@12", "m3001_weather");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
     $currentHourInNepal = getCurrentTimestampInHours();
-
     $existingWeatherData = getWeatherDataFromDatabase($conn, $city);
 
     //check if city exists in the database
@@ -158,6 +156,7 @@ if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["city"])) {
                         "historical_weather" => $historyData
                     ];
                 } else {
+                    //database failed to give data
                     $response_data = ["status" => "error", "message" => "Failed to retrieve stored data from the database"];
                 }
             }
@@ -187,18 +186,20 @@ if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["city"])) {
                     ],
                     "historical_weather" => $historyData
                 ];
+                //database failed to give data
             } else {
                 $response_data = ["status" => "error", "message" => "Failed to retrieve stored data from the database"];
             }
+            //api failes to respond
         } else {
             $response_data = ["status" => "error", "message" => "Failed to fetch data from API. No data available in the database."];
         }
     }
 
     $conn->close();
-
     header('Content-Type: application/json');
     echo json_encode($response_data);
+    //no city name given from JS
 } else {
     echo json_encode(["status" => "error", "message" => "Invalid request"]);
 }
